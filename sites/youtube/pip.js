3750fe73c82cc25671391c7db2c70d4bb383ae7f
ZsgsDesign <zsgsdesign@gmail.com>
2019-01-29 14:46:25 +0800

    add youtube support

--- sites/youtube/pip.js
+++ sites/youtube/pip.js
@@ -4,5 +4,14 @@ function parseDom(arg) {
     return objE.childNodes;
 };
 
-var htmlDOM = parseDom('<button class="ytp-pip-button ytp-button" aria-label="画中画 (Alt+P)" title="画中画 (Alt+P)" style="vertical-align: top;text-align: center;"><svg height="100%" version="1.1" viewBox="0 0 32 32" width="100%" style="fill: #fff;width: 18px;"><path d="M2.667 2.667h18.667v18.667h-18.667v-18.667M29.333 10.667v18.667h-18.667v-5.333h2.667v2.667h13.333v-13.333h-2.667v-2.667h5.333z"></path></svg></button>');
+var htmlDOM = parseDom('<button onclick="pipActive()" class="ytp-pip-button ytp-button" aria-label="画中画 (Alt+P)" title="画中画 (Alt+P)" style="vertical-align: top;text-align: center;"><svg height="100%" version="1.1" viewBox="0 0 32 32" width="100%" style="fill: #fff;width: 18px;"><path d="M2.667 2.667h18.667v18.667h-18.667v-18.667M29.333 10.667v18.667h-18.667v-5.333h2.667v2.667h13.333v-13.333h-2.667v-2.667h5.333z"></path></svg></button>')[0];
 $(".ytp-right-controls").prepend(htmlDOM);
+
+
+function pipActive() {
+    chrome.extension.sendRequest({
+        action: "active"
+    }, function (response) {
+        console.log("PIP succeed.");
+    });
+}
